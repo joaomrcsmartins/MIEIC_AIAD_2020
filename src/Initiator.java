08df4363ff4d0129a05ee39b8b9392bbf6ce75d8
João Martins <up201707311@fe.up.pt>
2020-10-16 14:20:39 +0100

    added jade and intellij config

---
+++ src/Initiator.java
@@ -0,0 +1,17 @@
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+
+public class Initiator {
+    public static void main(String[] args) throws StaleProxyException {
+        Runtime rt = Runtime.instance();
+        Profile profile = new ProfileImpl();
+        profile.setParameter(profile.GUI,"true");
+        ContainerController ac = rt.createMainContainer(profile);
+        AgentController agc = ac.createNewAgent("joao","seller",null);
+        agc.start();
+    }
+}
